aa603671534b7284866e3f2bb476bd01e5bc08f0
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2025-10-07 17:47:30 +0800

    Revert "missage"

--- backend/app/Views/user/landingPage.php
+++ backend/app/Views/user/landingPage.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
     <title>Batis Point | Your Private Camping Escape</title>
+=======
+    <title>Batis Points | Your Private Camping Escape</title>
+>>>>>>> parent of 69ee3ae (missage)
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body {
