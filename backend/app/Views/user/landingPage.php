f2c363c5ead6565a2dd5edb6f867959087e2bedb
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2025-10-08 00:11:13 +0800

    Update landingPage.php

--- backend/app/Views/user/landingPage.php
+++ backend/app/Views/user/landingPage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Batis Pointabjbjbjj | Your Private Camping Escape</title>
+    <title>Batis Point | Your Private Camping Escape</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         body {
