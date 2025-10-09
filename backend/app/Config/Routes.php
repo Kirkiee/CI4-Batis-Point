560b96c1943ff5fcebaa8c7c84efa021dabf3ec0
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2025-10-09 23:52:44 +0800

    feat(frontend/employeeCreationPage) - Added routes, users and page for employee creation

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -9,3 +9,4 @@ $routes->get('/', 'Users::index');
 $routes->get('/loginPage', 'Users::login');
 $routes->get('/moodboardPage', 'Users::moodboard');
 $routes->get('/roadmapPage', 'Users::roadmap');
+$routes->get('/employeeCreationPage', 'Users::employeeCreation');
